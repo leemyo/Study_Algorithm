27cb28c5e235d0ff95a2d6d5bae28568e517fdf2
leemyo <leemyo_@naver.com>
2021-02-15 14:02:19 +0900

    Delete 폰켓몬.py

--- 폰켓몬.py
+++
@@ -1,9 +0,0 @@
-def solution(nums):
-    kind=set(nums)
-    if len(nums)/2<=len(kind): return len(nums)/2
-    else: return len(kind)
-
-    """
-    설명
-    https://blog.naver.com/leemyo_/222244097375
-    """
